d5789f37cced548418cad0f40d56b2416b968e90
Asish Binu Mathew <asishbm98@gmail.com>
2023-04-18 15:01:13 +0100

    Font fix:

--- src/Main.js
+++ src/Main.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Main() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="lg:font-Inter lg:text-white lg:font-bold lg:text-9xl lg:mt-64 mt-32 font-Inter text-white font-bold text-5xl">
+      <h1 className="font-Inter lg:text-white lg:font-bold lg:text-9xl lg:mt-64 mt-32 font-Inter text-white font-bold text-5xl">
         Coming Soon
       </h1>
-      <h2 className="textCenterContent py-4 lg:text-4xl text-2xl">
+      <h2 className="textCenterContent py-4 lg:text-4xl text-2xl font-Inter">
         We are currently working on creating something fantastic. Subscribe to
         be notified
       </h2>
